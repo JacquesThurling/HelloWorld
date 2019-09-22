be7fcd2e522a6b86d66d6325f247872852f6c8d4
JacquesThurling <50457659+JacquesThurling@users.noreply.github.com>
2019-09-22 23:28:38 +0200

    Create HelloWorld.py
    First Python Code

---
+++ HelloWorld.py
@@ -0,0 +1,22 @@
+print('Hello World!')
+
+
+greeting = "Hello"
+name = input("Please enter your name ")
+print(greeting + " " + name)
+
+splitString = "This string has been \nsplit over \nseveral \nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4"
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting')
+print("The pet shop owner said \"No")
+
+anotherSplitString = """This string has been
+split over
+three line"""
+print(anotherSplitString)
+
+print("""The pet shop owner said "No, no uh he's resting.""")
